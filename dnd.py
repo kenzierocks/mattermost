16a1640516460a54734d580a2a18650ae120a7fc
Kenzie Togami <ket1999@gmail.com>
2016-03-18 01:10:44 -0400

    Refactor commands, add listcmd

--- dnd.py
+++ dnd.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, Response, request, jsonify, abort
+from flask import Flask, Response, request, jsonify, abort, url_for
 from tinydb import TinyDB, Query
 from tinydb.operations import increment
 import sys
@@ -89,35 +89,84 @@ def index():
             return msg("An unknown error occurred processing your input")
     return no_msg()
 
+@app.route('/listcmd')
+def listcmd():
+    l = []
+    for k, v in command_map.items():
+        l.append("++{}: {}".format(k, v['desc']))
+    return '<br>'.join(x.replace('<', '&lt;').replace('>', '&gt;') for x in l)
+
+command_map = dict()
+
+def command(name, desc, arg_list, admin=False, aliases=[]):
+    def insert_f(f):
+        command_map[name] = {
+                'func': f,
+                'desc': desc + ' Args: ' + args_to_string(arg_list),
+                'args': arg_list,
+                'admin': admin
+        }
+        return f
+    return insert_f
+
+def args_to_string(args):
+    if args is None:
+        return 'Unknown.'
+    s = '['
+    for x in range(len(args)):
+        s += args[x]
+        if x < (len(args) - 1):
+            s += ', '
+    return s + ']'
+
+
+@command('add_alias', 'Adds an alias to an ident.', ['ident', 'alias'], True)
+def c_add_alias(ident, alias):
+    return aliases.add_alias(ident, alias)
+
+@command('merge_idents', 'Merges ident_from into ident_to.', ['ident_from', 'ident_to'], True)
+def c_merge_idents(ident_from, ident_to):
+    return merge_ident(i_from, ident_to)
+
+@command('winner', 'Finds the user with the most points.', [])
+def c_winner():
+    top = get_largest_entry()
+    return 'Current top score: {} with {}'.format(top['ident'], top['count'])
+
+@command('get', 'Finds the score for the given user.', ['name'])
+def c_get(name):
+    count = get_entry(ident_or_by_name(name))
+    return "{}'s score is {}".format(name, count)
+
+@command('help', 'Lists help for all or a specified command.', None)
+def c_help(args):
+    if len(args) == 0:
+        return "Visit {} for a full list of commands.".format(request.url_root + 'listcmd')
+    cmd = args[0]
+    if cmd not in command_map:
+        return "The command {} does not exist.".format(cmd)
+    return "```\n++{}: {}\n```".format(cmd, command_map[cmd]['desc'])
 
 def handle_command(parts):
     is_admin = admin.is_admin(request.form['user_id'])
-    if parts[0] == 'add_alias' and is_admin:
-        if len(parts) < 3:
-            return msg("Need 2 args: [ident, alias]")
-        ident = parts[1]
-        alias = parts[2]
-        r = aliases.add_alias(ident, alias)
+    cmd_name = parts[0]
+    if cmd_name in command_map:
+        cmd_data = command_map[cmd_name]
+        if cmd_data['admin'] and not is_admin:
+            return msg("You don't have permission to run {}".format(cmd_name))
+        if cmd_data['args'] is not None and  len(parts) < (1 + len(cmd_data['args'])):
+            return msg("Need {} arg(s): {}".format(len(cmd_data['args']), args_to_string(cmd_data['args'])))
+        args = parts[1:]
+        r = None
+        if cmd_data['args'] is None:
+            r = cmd_data['func'](args)
+        else:
+            r = cmd_data['func'](*args)
         if r:
-            return msg(r)
+            if isinstance(r, str):
+                return msg(r)
+            return r
         return no_msg()
-    if parts[0] == 'merge_idents' and is_admin:
-        if len(parts) < 3:
-            return msg("Need 2 args: [ident_from, ident_to]")
-        i_from = parts[1]
-        i_to = parts[2]
-        r = merge_ident(i_from, i_to)
-        if r:
-            return msg(r)
-        return no_msg()
-    if parts[0] == 'winner':
-        top = get_largest_entry()
-        return msg('Current top score: {} with {}'.format(top['ident'], top['count']))
-    if parts[0] == 'get':
-        if len(parts) < 2:
-            return msg("Need 1 arg: [name]")
-        count = get_entry(ident_or_by_name(parts[1]))
-        return msg("{}'s score is {}".format(parts[1], count))
     return msg("Unknown ++ command " + parts[0])
 
 
